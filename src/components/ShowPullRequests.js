4b42cc3fe1427792fbad4a2c8130398e0f93fa7d
J Lim <jeong.s.lim@gmail.com>
2017-11-16 18:50:51 -0800

    Save WIPO

--- src/components/ShowPullRequests.js
+++ src/components/ShowPullRequests.js
@@ -19,6 +19,8 @@ const enhance = compose(
         .then(resp => resp.json())
         .then((resp) => {
           this.setState({ apiData: flattenDeep(resp) });
+        }).catch((e) => {
+          this.setState({ error: e });
         });
     }
   }),
@@ -30,12 +32,15 @@ const enhance = compose(
 );
 
 const ShowPullRequests = enhance(({
-  apiData, selectedFilter, updateFilter
+  apiData, selectedFilter, updateFilter, error
 }) => (
   <div id="show-prs">
     <NavBar selected={selectedFilter} updateFilter={updateFilter} />
     {apiData &&
       <JSONTree data={apiData} theme={JSONtheme} invertTheme={true} />
+    }
+    {error &&
+      <h1 style={{ color: 'red' }}>Something bad happened.</h1>
     }
   </div>
 ));
